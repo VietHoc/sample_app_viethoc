43faf8cc6fa027632207e3174e2da245a027f807
viethoc <viethocpro@gmail.com>
2017-03-16 15:35:37 +0700

    done

---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+User.create!(name:  "hoc",
+             email: "viethocpro@gmail.com",
+             password:              "viethoc1",
+             password_confirmation: "viethoc1",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::ChuckNorris.fact
+  users.each { |u| u.microposts.create!(content: content) }
+end
+
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
